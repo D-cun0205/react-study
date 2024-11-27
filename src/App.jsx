20f4d14367fd3259231d1a03261d3d47cf71f8f1
박상훈 <psh01@univs.ai>
2024-11-27 20:27:36 +0900

    create repeated component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -11,7 +14,7 @@ function Header() {
 
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {description} React concepts you will need for almost any app you are
@@ -21,14 +24,32 @@ function Header() {
     );
 }
 
+function CoreConcept({ image, title, description }) {
+    return (
+      <li>
+          <img src={image} alt={title} />
+          <h3>{title}</h3>
+          <p>{description}</p>
+      </li>
+    );
+}
+
 // If you want to use the Header component in the App component, you need to wrap it with '<' and '>' tags.
 // Also, the component must be inside a JSX fragment.
 function App() {
     return (
         <div>
             <Header /> {/* or <Header></Header> */}
-
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
